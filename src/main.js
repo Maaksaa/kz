eafaa4d8713764becc65c86b629d60210cd5709b
Maksim Demidov <maaksaa@gmail.com>
2022-04-03 23:26:44 +0300

    Подключил bootstrap-vue и добавил элементы

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue } from 'bootstrap-vue'
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
 
 new Vue({
   render: function (h) { return h(App) },
